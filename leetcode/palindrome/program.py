2f1c08986e1640a2da893380945e190c72a736bc
artemise1tara <ahmedsecen2@gmail.com>
2025-10-17 00:43:00 -0400

    misc

--- leetcode/palindrome/program.py
+++ leetcode/palindrome/program.py
@@ -1,4 +1,31 @@
 import math
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = int(input('Input a number: '))
+        if x<0:
+            return(False)
+        elif x<10:
+            return(False)
+        else:
+            return str(x)==str(x)[::-1]
+    
+    def helloWorld(self, y: float) -> bool:
+        pi = math.pi
+        if y>=pi:
+            return y==7
+        else:
+            return(False)
+class Calculation:
+    def squareRoot(self, n: float):
+        if n<0:
+            return('Cannot square root negative numbers.')
+        else:
+            return(round(math.sqrt(n), 4))
+            
+            
+x = int(input("Integer input: "))
+y = float(input('Float input: '))
+n = float(input('Number to be square rooted: '))
+print(f'Is palindrome: {Solution().isPalindrome(x)}')
+print(f'Hello world: {Solution().helloWorld(y)}')
+print(f'Square root: {Calculation().squareRoot(n)}')
