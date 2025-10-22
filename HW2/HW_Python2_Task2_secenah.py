c643f05252ae533f2dd456f2cdbaddc5b463d545
artemise1tara <ahmedsecen2@gmail.com>
2025-10-22 14:07:44 -0400

    misc

--- HW2/HW_Python2_Task2_secenah.py
+++ HW2/HW_Python2_Task2_secenah.py
@@ -17,15 +17,19 @@
 # blood tests that the user inputs, put the patient in (Normal, High Risk, or Diabetic).
 
 import math
+
+#define user inputs
 blood_glucose = float(input("Enter the fasting blood glucose level: "))
 hemoglobin = float(input("Enter the hemoglobin A1c level as a percentage without the sign: "))
 class Solution:
-
+    #define method for categorizing based on fasting blood glucose levels
     def calcBloodGlucose(self):
+        #checks for negative numbers
         if blood_glucose<0:
             return('Invalid fasting blood glucose value')
         elif hemoglobin<0:
             return('Invalid hemoglobin A1c level')
+        #sets categories to integers for comparison purposes
         else:
             if blood_glucose<100:
                 bloodCategory = 0
@@ -33,13 +37,17 @@ class Solution:
                 bloodCategory = 1
             else:
                 bloodCategory = 2
+            #returns the category number
             return(int(bloodCategory))
-    
+        
+    #define method for categorizing based on hemoglobin A1c levels
     def calcHemoglobin(self):
+        #checks for negative numbers
         if blood_glucose<0:
             return('Invalid fasting blood glucose value')
         elif hemoglobin<0:
             return('Invalid hemoglobin A1c level')
+        #sets categories to integers for comparison purposes
         else:
             if hemoglobin<5.7:
                 hemoglobinCategory = 0
@@ -47,11 +55,15 @@ class Solution:
                 hemoglobinCategory = 1
             else:
                 hemoglobinCategory = 2
+            #returns the category number
             return(int(hemoglobinCategory))
 
+#class for determining order of precedence for risk severity
 class Precedence: 
     def determinePrecedence(self):
+        #checks if the blood glucose level category is greater based on the integer value
         if int(Solution().calcBloodGlucose()) > int(Solution().calcHemoglobin()):
+            #redefines the risk categories as strings
             if int(Solution().calcBloodGlucose()) == 0:
                 final = 'Normal'
                 return(final)
@@ -61,8 +73,10 @@ class Precedence:
             elif int(Solution().calcBloodGlucose()) == 2:
                 final = 'Diabetic'
                 return(final)
+        #checks if the hemoglobin A1c category is greater based on the integer value
         else:
             if int(Solution().calcHemoglobin()) > int(Solution().calcBloodGlucose()):
+                #redefines the risk categories as strings
                 if int(Solution().calcHemoglobin()) == 0:
                     final = 'Normal'
                     return(final)
@@ -73,8 +87,8 @@ class Precedence:
                     final = 'Diabetic'
                     return(final)
             
-            
-print(Precedence().determinePrecedence())
+#prints the output of 'precedence' as a string
+print(f'Patient is: {Precedence().determinePrecedence()}')
             
             
 
