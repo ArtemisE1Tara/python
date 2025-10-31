0a5104493f7bdf45f9a0307eba44380251b4dce9
artemise1tara <ahmedsecen2@gmail.com>
2025-10-31 10:57:52 -0400

    add comments

--- repetition flow 1/ACT_Python_3p4_secenah.py
+++ repetition flow 1/ACT_Python_3p4_secenah.py
@@ -1,27 +1,52 @@
+# PYTHON 3: REPETITION FLOW (CONDITIONAL STRUCTURES) --> Task 4
+# File: ACT_Python_3p4secenah.py
+# Date:    10/31/2025
+# By:      Ahmed Secen
+# Section: 018
+# Team:    2
+# 
+# ELECTRONIC SIGNATURE
+# Ahmed Secen
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# This script determines the winner of a set number of dice games based on how many rounds they won.
+
 import random
 
+#get player name inputs
 player1 = input('Player 1 name: ')
 player2 = input('Player 2 name: ')
+#get number of rounds to be played
 x = float(input('Number of rounds to be played: '))
 
+#initialize variables
 roundCount = 0
 player_1_winCount = 0
 player_2_winCount = 0
 
+#set condition
 while roundCount<=x:
+    #get random player roll values
     player_1_rollValue = random.randint(1,6)
     player_2_rollValue = random.randint(1,6)
 
+    #update player win counts based on who won
     if player_1_rollValue>player_2_rollValue:
         player_1_winCount = player_1_winCount + 1
     else:
         player_2_winCount = player_2_winCount + 1
     roundCount = roundCount + 1
 
+#determine winners
 if player_1_winCount>player_2_winCount:
     print(f'{player1} wins!')
 elif player_2_winCount>player_1_winCount:
     print(f'{player2} wins!')
+#determines tie
 elif player_2_winCount>player_1_winCount:
     print(f"It's a tie!")
 
--- repetition flow 1/ACT_Python_3p5_secenah.py
+++ repetition flow 1/ACT_Python_3p5_secenah.py
@@ -1,27 +1,51 @@
-import random
+# PYTHON 3: REPETITION FLOW (CONDITIONAL STRUCTURES) --> Task 5
+# File: ACT_Python_3p5secenah.py
+# Date:    10/31/2025
+# By:      Ahmed Secen
+# Section: 018
+# Team:    2
+# 
+# ELECTRONIC SIGNATURE
+# Ahmed Secen
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# This script determines the winner of multiple rounds of dice based on who 
+# reaches or exceeds a target score first.
 
+import random
+#get score input
 targetValue = float(input('Enter target score: '))
 
+#get player name inputs
 player1 = input('Player 1 name: ')
 player2 = input('Player 2 name: ')
 
+#initialize variables
 player1_rollValue = 0
 player2_rollValue = 0
 player1_total = 0
 player2_total = 0
 
+#set conditions
 while player1_total<targetValue and player2_total<targetValue:
+    #get random roll values
     player1_rollValue = random.randint(1,6)
     player2_rollValue = random.randint(1,6)
 
+    #update player score totals
     player1_total = player1_rollValue + player1_total
     player2_total = player2_rollValue + player2_total
 
+#determine winners
 if player1_total>player2_total:
     print(f"{player1} wins!\n{player1}'s score: {player1_total}\n{player2}'s score: {player2_total}")
 elif player2_total>player1_total:
     print(f"{player2} wins!\n{player2}'s score: {player2_total}\n{player1}'s score: {player1_total}")
+#determines tie
 elif player2_total==player1_total:
     print("It's a tie!\n{player1}'s score: {player1_total}\n{player2}'s score: {player2_total}")
-
 
