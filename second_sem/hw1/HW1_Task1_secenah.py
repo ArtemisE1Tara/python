a70b06ea32d31bb431e29bd2dfda2bd2d9f19448
ArtemisE1Tara <ahmedsecen2@gmail.com>
2026-01-25 22:06:00 -0500

    hw1

---
+++ second_sem/hw1/HW1_Task1_secenah.py
@@ -0,0 +1,32 @@
+import random
+
+class Solution:
+    def Reliability(self):
+        simNum = int(input("Enter simulation count: "))
+        while simNum<=0:
+            simNum = int(input("Invalid input. Simulation count must be greater than zero.\nEnter simulation count: "))
+        
+        processNum = int(input("Enter component process count: "))
+        while processNum<=0:
+            processNum = int(input("Invalid input. Component process count must be greater than zero.\nEnter component process count: "))
+        
+        compRel = []
+        for i in range(1, processNum+1):
+            Rel = float(input(f"Enter reliability of component {i}: "))
+
+            while Rel<0 or Rel>1:
+                Rel = float(input(f"Invalid input. Reliability must be in range 0-1.\nEnter reliability of component {i}: "))
+            else:
+                compRel.append(Rel)
+        
+        failCount = 0
+        for x in range(simNum+1):
+            for y in range(len(compRel)):
+                if compRel[y]<random.random():
+                    failCount = failCount + 1
+        
+        percentFail = (failCount/simNum)*100
+
+        print(f"{percentFail}% of simulations failed.")
+
+Solution().Reliability()
---
+++ second_sem/hw1/HW1_Task2_secenah.py
@@ -0,0 +1,22 @@
+import math
+
+List = [[0.04, -0.01, 0.03, 0.02, 0.03], 
+        [0.02, 0.01, 0.03, -0.01, 0.00], 
+        [0.06, 0.07, -0.03, 0.05, 0.04],
+        [-.02, -0.03, 0.01, 0.03, 0.02]]
+
+A = 0.557
+
+avgSumList = []
+
+# x_1 = sum(List[0])/len(List[0])
+# x_2 = sum(List[1])/len(List[1])
+# x_3 = sum(List[2])/len(List[2])
+# x_4 = sum(List[3])/len(List[3])
+
+for i in range(len(List)):
+    key_name = f"x_{i}"
+
+
+    
+
