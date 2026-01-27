6d7e543fb276a47dd90c6cf981c9d29d6606de7b
artemise1tara <ahmedsecen2@gmail.com>
2026-01-27 16:49:08 -0500

    add headers

--- second_sem/hw1/HW1_Task1_secenah.py
+++ second_sem/hw1/HW1_Task1_secenah.py
@@ -1,7 +1,26 @@
+# HW1: Task 1
+# File: HW1_Task1_secenah.py
+# Date:    01/27/2026
+# By:      Ahmed Secen
+# Section: 008
+# 
+# Ahmed Secen
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# This script determines the percentage of reliability simulations failed.
+
+#get imports
 import random
 
+#define class
 class Solution:
+    #define method
     def Reliability(self):
+        #get inputs
         simNum = int(input("Enter simulation count: "))
         while simNum<=0:
             simNum = int(input("Invalid input. Simulation count must be greater than zero.\nEnter simulation count: "))
@@ -10,7 +29,10 @@ class Solution:
         while processNum<=0:
             processNum = int(input("Invalid input. Component process count must be greater than zero.\nEnter component process count: "))
         
+        #initialize reliability list
         compRel = []
+
+        #set loop for entering reliability values
         for i in range(1, processNum+1):
             Rel = float(input(f"Enter reliability of component {i}: "))
 
@@ -19,17 +41,22 @@ class Solution:
             else:
                 compRel.append(Rel)
         
+        #initialize fail counter
         failCount = 0
+        #setup loop for simulation runs
         for x in range(simNum+1):
             simFail = 0
+            #iterate through reliability values
             for y in range(len(compRel)):
                 if compRel[y]<random.random():
                     simFail = 1
             if simFail == 1:
+                #update fail counter
                 failCount = failCount + 1
-        
+        #caluclate failure rate percentage
         percentFail = (failCount/simNum)*100
-
+        #print output
         print(f"{percentFail}% of simulations failed.")
 
+#run program
 Solution().Reliability()
--- second_sem/hw1/HW1_Task2_secenah.py
+++ second_sem/hw1/HW1_Task2_secenah.py
@@ -1,36 +1,63 @@
+# HW1: Task 2
+# File: HW1_Task2_secenah.py
+# Date:    01/27/2026
+# By:      Ahmed Secen
+# Section: 008
+# 
+# Ahmed Secen
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# This script determines if a process is in control based on whether 
+# or not each sample range average is iwthin upper and lower control limits.
+
+#get imports
 import math
 
+#hardcode sample value 2D list
 List = [[0.04, -0.01, 0.03, 0.02, 0.03], 
         [0.02, 0.01, 0.03, -0.01, 0.00], 
         [0.06, 0.07, -0.03, 0.05, 0.04],
         [-.02, -0.03, 0.01, 0.03, 0.02]]
 
+#hardcode variable value
 A = 0.557
 
+#initialize sample averages list
 avgSumList = []
 
+#computes averages
 X1 = sum(List[0])/len(List[0])
 X2 = sum(List[1])/len(List[1])
 X3 = sum(List[2])/len(List[2])
 X4 = sum(List[3])/len(List[3])
 
+#compute average of averages
 x_avg = (X1+X2+X3+X4)/4
 
+#compute sample value ranges
 R1 = max(List[0])-min(List[0])
 R2 = max(List[1])-min(List[1])
 R3 = max(List[2])-min(List[2])
 R4 = max(List[3])-min(List[3])
 
+#compute average of sample value ranges
 R_avg = (R1+R2+R3+R4)/4
 
+#compute upper and lower control limits
 UCL = x_avg + (A*R_avg)
 LCL = x_avg - (A*R_avg)
 
+#set initial booleans to False
 X1_pass = False
 X2_pass = False
 X3_pass = False
 X4_pass = False
 
+#check if sample averages are within limits
 if X1<UCL and X1>LCL:
     X1_pass = True
 
@@ -43,6 +70,7 @@ if X3<UCL and X3>LCL:
 if X4<UCL and X4>LCL:
     X4_pass = True
 
+#check if whole process is in control
 if X1_pass==X2_pass==X3_pass==X4_pass:
     print("Process is in control.")
 else:
