592d58134f2eb5356d5e314cb01013c42209b67c
artemise1tara <ahmedsecen2@gmail.com>
2026-02-05 14:49:27 -0500

    test2

--- second_sem/wk4/ACT_Python6p1_secenah.py
+++ second_sem/wk4/ACT_Python6p1_secenah.py
@@ -1,5 +1,21 @@
-file = open('TempC.txt', 'r')
+import os
 
-content = file.readlines()
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'TempC.txt')
+output_path = os.path.join(script_dir, 'TempF.txt')
 
-print(content)
+# Read temps from file
+with open(file_path, 'r') as file:
+    content = file.readlines()
+
+# Convert celsius to new temp
+floatList = [float(temp.strip()) for temp in content]
+newTemp = []
+for tempC in floatList:
+    tempF = round(((tempC * 1.8) + 32))
+    newTemp.append(tempF)
+
+# Write new temperatures to file
+with open(output_path, 'w') as new:
+    for i in newTemp:
+        new.write(f"{i}\n")
